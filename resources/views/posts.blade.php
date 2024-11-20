8cf43d22a8de1e5670452e170b055b84d10a21d0
ethanvue002 <ethan.vue.753@my.csun.edu>
2024-11-20 13:08:13 -0800

    updated posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,49 @@
-<!-- resources/views/test.blade.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Page</title>
-</head>
-<body>
-    <h1>Hello, welcome to the Test Page!</h1>
-    <p>This is a simple Blade view.</p>
-    <a href="{{ route('dashboard') }}">
-        <button type="button">Back to Dashboard</button>
-    </a>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <form action="/p" enctype="multipart/form-data" method="post">
+        @csrf
+
+        <div class="row">
+            <div class="col-8 offset-2">
+
+                <div class="row">
+                    <h1>Add New Post</h1>
+                </div>
+                <div class="form-group row">
+                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+
+                    <input id="caption"
+                           type="text"
+                           class="form-control{{ $errors->has('caption') ? ' is-invalid' : '' }}"
+                           name="caption"
+                           value="{{ old('caption') }}"
+                           autocomplete="caption" autofocus>
+
+                    @if ($errors->has('caption'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('caption') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+
+                    <input type="file" class="form-control-file" id="image" name="image">
+
+                    @if ($errors->has('image'))
+                        <strong>{{ $errors->first('image') }}</strong>
+                    @endif
+                </div>
+
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Add New Post</button>
+                </div>
+
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
