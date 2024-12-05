12711a0b3b51a63addde28df3b890176d646c85f
javi <javier.reyes.732@my.csun.edu>
2024-12-05 15:36:39 -0800

    did css for welcome/landing page & also for fetch page, fixed mminor bugs along the way.

--- resources/views/recipes/fetch.blade.php
+++ resources/views/recipes/fetch.blade.php
@@ -1,24 +1,40 @@
-<!-- resources/views/recipes/fetch.blade.php -->
 @extends('layouts.app')
 
 @section('title', 'Recipe Details')
 
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/recipe-details.css') }}">
+@endpush
+
 @section('content')
 <div class="container">
-    <h1>{{ $recipe['title'] }}</h1>
-    
-    <img src="{{ $recipe['image'] }}" alt="{{ $recipe['title'] }}" class="img-fluid">
+    <!-- Top Section (Image and Ingredients) -->
+    <div class="top-section">
+        <!-- Image Section -->
+        <div class="image-section">
+            <img src="{{ $recipe['image'] }}" alt="{{ $recipe['title'] }}">
+        </div>
 
-    <h3>Ingredients</h3>
-    <ul>
-        @foreach($recipe['extendedIngredients'] as $ingredient)
-            <li>{{ $ingredient['original'] }}</li>
-        @endforeach
-    </ul>
+        <!-- Ingredients Section -->
+        <div class="ingredients-section">
+            <h3>Ingredients</h3>
+            <ul class="ingredients-list">
+                @foreach($recipe['extendedIngredients'] as $ingredient)
+                    <li>{{ $ingredient['original'] }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 
-    <h3>Instructions</h3>
-    <p>{{ $recipe['instructions'] }}</p>
+    <!-- Instructions Section -->
+    <div class="instructions-section">
+        <h3>Instructions</h3>
+        <ol>
+            {!! nl2br(e($recipe['instructions'])) !!}
+        </ol>
+    </div>
 
-    <a href="{{ route('recipes.index') }}" class="btn btn-primary">Back to Search</a>
+    <!-- Back Button -->
+    <a href="{{ route('recipes.index') }}" class="btn">Back to Search</a>
 </div>
 @endsection
